e30f620c2a5a775c7ba839c8bd41ff74db91abc3
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-01-31 22:57:24 +1300

    fingers crossed

--- web/index.php
+++ web/index.php
@@ -2,19 +2,19 @@
 // remove before flight
 ini_set('display_errors', 'On');
 
-// require('../vendor/autoload.php');
+require('../vendor/autoload.php');
 
-// use Aws\S3\S3Client;
+use Aws\S3\S3Client;
 
-// $options = [
-//     'region'            => 'ap-southeast-2',
-//     'version'           => 'latest',
-// ];
+$options = [
+    'region'            => 'ap-southeast-2',
+    'version'           => 'latest',
+];
 
-// $s3 = new S3Client($options);
+$s3 = new S3Client($options);
 
-// $bucket = getenv('S3_BUCKET')?: 
-// die('No "S3_BUCKET" config var in found in env!');
+$bucket = getenv('S3_BUCKET')?: 
+die('No "S3_BUCKET" config var in found in env!');
 
 try {
     $db = new PDO('pgsql:host=ec2-54-204-41-175.compute-1.amazonaws.com;port=5432;dbname=d6jmmjm506o0h9;user=ggamcflhqstetx;password=1jc95h0WehE3P8hvgnrQrx9rBT');  
@@ -30,7 +30,18 @@ try {
 		$test = pg_escape_string($_POST['hackTitle']); 
 		$sql = "INSERT INTO test_table (id, name) VALUES ('2', '" . $test . "')";
 	    // use exec() because no results are returned
-	    $db->exec($sql);	
+	    $db->exec($sql);
+	    
+	    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['pic']) && $_FILES['pic']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['pic']['tmp_name'])) {
+		    // FIXME: add more validation, e.g. using ext/fileinfo
+		    try {
+		        // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
+		        $upload = $s3->upload($bucket, $_FILES['pic']['name'], fopen($_FILES['pic']['tmp_name'], 'rb'), 'public-read');
+		    } catch (Exception $e) {
+			    echo $e->getMessage();
+			    die();
+			}
+		}	
 	}
 	// echo '<pre>';
 	// var_dump($results->fetchAll());
@@ -41,16 +52,7 @@ try {
     die();
 }
 
-// if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['pic']) && $_FILES['pic']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['pic']['tmp_name'])) {
-//     // FIXME: add more validation, e.g. using ext/fileinfo
-//     try {
-//         // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
-//         $upload = $s3->upload($bucket, $_FILES['pic']['name'], fopen($_FILES['pic']['tmp_name'], 'rb'), 'public-read');
-//     } catch (Exception $e) {
-// 	    echo $e->getMessage();
-// 	    die();
-// 	}
-// }
+
 
 $hacks = $results->fetchAll(PDO::FETCH_ASSOC);
 
