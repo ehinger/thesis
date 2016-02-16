a5c5100c55782ddabc05adf1c8e583edb57630df
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-02-16 15:58:24 +1300

    doin image stuff still

--- web/index.php
+++ web/index.php
@@ -130,7 +130,6 @@ Post a hack
 
 ************************************************************************************/ -->
 
-<h1>S3 upload example</h1>
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
     // FIXME: add more validation, e.g. using ext/fileinfo
