bbd745f614eef4b3edda244edc3c30f764ae6240
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-02-16 15:01:32 +1300

    doin image stuff still

--- web/index.php
+++ web/index.php
@@ -143,9 +143,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
         <p>Upload error :(</p>
 <?php } } ?>
         <h2>Upload a file</h2>
-<!--         <form enctype="multipart/form-data" action="<?=$_SERVER[]?>" method="POST">
+        <form enctype="multipart/form-data" action="<?=$_SERVER[]?>" method="POST">
             <input name="userfile" type="file"><input type="submit" value="Upload">
-        </form> -->
+        </form>
 
 <div class='newHackFrame'>
 
