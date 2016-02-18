4f15de7b281ad98bd71cc5a0fc595dcdf4a5cf73
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-02-18 13:01:54 +1300

    doin image stuff still

--- web/index.php
+++ web/index.php
@@ -41,6 +41,17 @@ try {
 //     $identification .= mt_rand(0,9);
 // }
 
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+    // FIXME: add more validation, e.g. using ext/fileinfo
+    try {
+        // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
+        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+    } catch(Exception $e) { 
+        echo $e->getMessage();
+        die();
+    } 
+} 
+
 try {
     $results1 = $db->query('select * from hacksdesc');
     $results2 = $db->query('select * from hacksingredients');
@@ -107,16 +118,7 @@ Post a hack
 ************************************************************************************/ -->
 
 <?php
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-    // FIXME: add more validation, e.g. using ext/fileinfo
-    try {
-        // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
-        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
-    } catch(Exception $e) { 
-        echo $e->getMessage();
-        die();
-    } 
-} 
+
 ?>
 
 <div class='newHackFrame'>
