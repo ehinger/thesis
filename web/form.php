f7a3b366756c7829b27537e75231e82cb9183521
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-02-22 00:48:07 +1300

    doin image stuff still

--- web/form.php
+++ web/form.php
@@ -37,7 +37,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
         echo "File is not an image.";
         $uploadOk = 0;
     }
-    
+
     // Check file size
     if ($target_file > 500000) {
         echo "Sorry, your file is too large.";
@@ -55,9 +55,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
         die();
     // if everything is ok, try to upload file
     } else {
+        rename($_FILES['userfile']['name'], "humn");
         try {
             // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
-            $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+            $upload = $s3->upload($bucket, "humn", fopen("humn", 'rb'), 'public-read');
         } catch(Exception $e) { 
             echo $e->getMessage();
             die();
