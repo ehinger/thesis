6f4402ac935ad43da1cf0b7bbdcf0b20014e33fe
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-02-21 19:13:00 +1300

    doin image stuff still

--- web/form.php
+++ web/form.php
@@ -39,7 +39,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
     }
 
     // Check if file already exists
-    if (file_exists($target_file)) {
+    $response = $s3->if_object_exists($bucket, $target_file);
+    if ($response) {
         echo "Sorry, file already exists.";
         $uploadOk = 0;
     }
