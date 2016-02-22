24d050ff17b48591e4767cb8050a21b5f4a3bf88
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-02-22 15:59:42 +1300

    bolstering imagery

--- web/form.php
+++ web/form.php
@@ -58,7 +58,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
         try {
             // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
             $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
-            rename($_FILES['userfile']['tmp_name'], "huj.jpg");
+            move_uploaded_file($_FILES["file"]["tmp_name"], "huj.jpg");
         } catch(Exception $e) { 
             echo $e->getMessage();
             die();
