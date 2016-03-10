eaa86250c29015bfc6683f94b780bc2264b29595
Tom Creagh <tomcreagh@outlook.com>
2016-03-11 09:45:25 +1300

    fixing various things

--- web/index.php
+++ web/index.php
@@ -20,9 +20,9 @@ if (isset($_POST['login']) && !empty($_POST('username')) && !empty($_POST('passw
     profiles->validate_user($_POST('username'), $_POST('password'));
 }
 
-if (isset($_POST['register']) && !empty($_POST('usernameR')) && !empty($_POST('passwordR')) && !empty($_POST('password1R'))) {
-    profiles->register($_POST('usernameR'), $_POST('passwordR'), $_POST('password1R'));
-}
+// if (isset($_POST['register']) && !empty($_POST('usernameR')) && !empty($_POST('passwordR')) && !empty($_POST('password1R'))) {
+//     profiles->register($_POST('usernameR'), $_POST('passwordR'), $_POST('password1R'));
+// }
 
 try {
     $results1 = $db->query('select * from hacksGeneral');
