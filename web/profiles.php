a89a31695610e701e116a63d2dcf88003539befd
Tom Creagh <tomcreagh@outlook.com>
2016-03-11 10:06:09 +1300

    fixing various things

--- web/profiles.php
+++ web/profiles.php
@@ -1,6 +1,28 @@
 <?php
 
-require_once "dbconn.php";
+// require_once "dbconn.php";
+
+require('../vendor/autoload.php');
+
+use Aws\S3\S3Client;
+
+$options = [
+    'region'            => 'ap-southeast-2',
+    'version'           => 'latest'
+];
+
+$s3 = new S3Client($options);
+
+$bucket = getenv('S3_BUCKET')?:
+die('No "S3_BUCKET" config var in found in env!');
+
+try {
+    $db = new PDO('pgsql:host=ec2-54-204-41-175.compute-1.amazonaws.com;port=5432;dbname=d6jmmjm506o0h9;user=ggamcflhqstetx;password=1jc95h0WehE3P8hvgnrQrx9rBT');  
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die();
+}
 
 class profiles {
 
