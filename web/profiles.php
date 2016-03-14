c914ddc9de58abee172fc1c2b4160a993aa61e7b
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-03-15 00:16:39 +1300

    bolstering

--- web/profiles.php
+++ web/profiles.php
@@ -29,7 +29,7 @@ class profiles {
 
 		$query = "SELECT FROM userProfile WHERE username = '" . $un . "' AND password = '" . $pwd . "' LIMIT 1";
 
-		$stmt = $db->prepare($query)) 
+		$stmt = $db->prepare($query); 
 		$stmt->execute();
 
 			if ($stmt->fetch()) {
