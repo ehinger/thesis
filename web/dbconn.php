06d4a15d07bce2b871e7ee22b68f28c8c607ac5f
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-03-14 23:25:01 +1300

    bolstering

--- web/dbconn.php
+++ web/dbconn.php
@@ -15,7 +15,7 @@ $bucket = getenv('S3_BUCKET')?:
 die('No "S3_BUCKET" config var in found in env!');
 
 try {
-    $db = new PDO('pgsql:host=ec2-54-204-41-175.compute-1.amazonaws.com;port=5432;dbname=d6jmmjm506o0h9;user=ggamcflhqstetx;password=1jc95h0WehE3P8hvgnrQrx9rBT');  
+    global $db = new PDO('pgsql:host=ec2-54-204-41-175.compute-1.amazonaws.com;port=5432;dbname=d6jmmjm506o0h9;user=ggamcflhqstetx;password=1jc95h0WehE3P8hvgnrQrx9rBT');  
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     echo $e->getMessage();
