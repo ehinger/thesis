1c13bf66300b305e0f87dc0615b9420d1f8c6ce2
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-03-15 16:06:53 +1300

    bolstering

--- web/index.php
+++ web/index.php
@@ -46,7 +46,7 @@ $hacks2 = $results2->fetchAll(PDO::FETCH_ASSOC);
 
 $abv = array('k' => , $hacks1);
 
-// print_r($abv['k']);
+echo $abv['k'];
 
 // $identification = '';
 // for ($i = 0; $i<7; $i++) 
