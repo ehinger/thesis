ec30451ebac1f1668cf10b00a4c673d046c555a4
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-03-30 01:15:21 +1300

    bolstering

--- web/profiles.php
+++ web/profiles.php
@@ -24,7 +24,7 @@ class profiles {
 		}
 	}
 
-	function verify_username_password ($un, $pwd, $id) {
+	function verify_username_password ($un, $pwd) {
 		global $db;
 
 		$query = "SELECT FROM userProfile WHERE username = '" . $un . "' AND password = '" . $pwd . "' LIMIT 1";
