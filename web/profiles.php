847011b72839e5700fecd71ee82bc15de326b575
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-03-30 17:05:46 +1300

    bolstering

--- web/profiles.php
+++ web/profiles.php
@@ -28,11 +28,13 @@ class profiles {
 		global $db;
 
 		// $query = "SELECT FROM userProfile WHERE username = '" . $un . "' AND password = '" . $pwd . "' LIMIT 1";
-
-		$stmt = $db->query("SELECT FROM userProfile WHERE username = '" . $un . "' AND password = '" . $pwd . "'"); 
-		$stmt->execute();
-				echo $un, $pwd;
-				die();
+}
+		// $stmt = $db->query("SELECT FROM userProfile WHERE username = '" . $un . "' AND password = '" . $pwd . "'"); 
+		// $stmt->execute();
+		$stmt = $db->query("SELECT FROM userProfile WHERE username = '" . $un . "' AND password = '" . $pwd . "'");
+		$d = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		echo $d;
+		die();
 			// if ($stmt->fetch()) {
 
 			// }
