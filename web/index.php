1b0739b8afc651dca8541d5b97e55ab1bd408e3a
Tom Creagh <tomcreagh@outlook.com>
2016-04-15 11:16:01 +1200

    fixing various things

--- web/index.php
+++ web/index.php
@@ -105,12 +105,18 @@ Navigation Bar
         <input type="submit" value="login" name="login">
 
     </form>
-
+    <?php
+if (isset($_COOKIE["userId"])) {
+    ?>
     <form id='logout' action='' method='post'>
 
         <input type="submit" value="logout" name="logout">
 
-    </form>
+    </form>        
+    <?php
+    }
+    ?>
+
 
     <form id='register' action='' method='post'>
 
