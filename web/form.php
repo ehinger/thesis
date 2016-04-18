549a642bee182759c38707fa7646245d2af310e1
Tom Creagh <tomcreagh@outlook.com>
2016-04-19 10:42:13 +1200

    fixing various things

--- web/form.php
+++ web/form.php
@@ -68,14 +68,14 @@ if (isset($_POST['push'])){
     // $check = getimagesize($_FILES["userfile"]["tmp_name"]);
     // $target_file = basename($_FILES["userfile"]["name"]);
     $uploadOk = 1;
-    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
+    // $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    // if($check !== false) {
+    //     echo "File is an image - " . $check["mime"] . ".";
+    //     $uploadOk = 1;
+    // } else {
+    //     echo "File is not an image.";
+    //     $uploadOk = 0;
+    // }
 
     // Check file size
     // if ($target_file > 5000000000) {
@@ -83,11 +83,11 @@ if (isset($_POST['push'])){
     //     $uploadOk = 0;
     // }
     // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
+    // if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    // && $imageFileType != "gif" ) {
+    //     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    //     $uploadOk = 0;
+    // }
     // foreach ($_FILES['userfile'] as $k => $v) {
 
     $nm = $_FILES['userfile']['name'];
