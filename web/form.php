ed9dfadcc254b88d278fb1448199a0939b678d98
Tom Creagh <tomcreagh@outlook.com>
2016-04-19 12:01:48 +1200

    fixing various things

--- web/form.php
+++ web/form.php
@@ -90,7 +90,7 @@ if (isset($_POST['push'])){
     // }
     // foreach ($_FILES['userfile'] as $k => $v) {
 
-    // for ($i = 0; $i < count($_FILES['userfile']['name']); $i++) {
+    for ($i = 0; $i < count($_FILES['userfile']['name']); $i++) {
 
     $nm = $_FILES['userfile']['name'];
     $tmpnm = $_FILES['userfile']['tmp_name'];
@@ -110,16 +110,15 @@ if (isset($_POST['push'])){
         try {
             // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
             // $upload = $s3->upload($bucket, $nm, "L", 'public-read');
-
             echo implode(" ", $_FILES['userfile']['name']);
+            echo implode(" ", $_FILES['userfile']['tmp_name']);
             die();
-            
         } catch(Exception $e) { 
             echo $e->getMessage();
             die();
         } 
     }
-// }
+}
 } 
 
 if (isset($_POST['followButton'])) {
@@ -132,4 +131,4 @@ if (isset($_POST['followButton'])) {
     }
 }
 
-// header('Location: index.php');
+header('Location: index.php');
