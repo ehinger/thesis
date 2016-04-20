d6f02668a517c95bb7db316daa50f08050cf9eab
Tom Creagh <tomcreagh@outlook.com>
2016-04-20 13:11:57 +1200

    fixing various things

--- web/profiles.php
+++ web/profiles.php
@@ -4,7 +4,7 @@ require_once "dbconn.php";
 
 class profiles {
 
-	function register ($un, $pwd, $pwd1, $fN, $lN, $pPic, $pPicN, $pPicTN) {
+	function register ($un, $pwd, $pwd1, $fN, $lN, $pPic, $pPicN) {
 		global $db;
 		global $bucket;
 		global $s3;
@@ -13,23 +13,23 @@ class profiles {
 		$uImgN = $pPicN;
 		$uImgTN = $pPicTN;
 
-		if (isset($uImg)) {
-			$upload = $s3->upload($bucket, $uImgN, "l", 'public-read');
-			$pro_pic = htmlspecialchars($upload->get('ObjectURL'));
-		}
+		// if (isset($uImg)) {
+		// 	$upload = $s3->upload($bucket, $uImgN, "l", 'public-read');, $pPicTN && isset($uImg)
+		// 	$pro_pic = htmlspecialchars($upload->get('ObjectURL'));
+		// }
 		
 		$un_register = pg_escape_string($un);
 		$pwd_register = pg_escape_string($pwd);
 		$pwd_check = pg_escape_string($pwd1);
 		$f_name = pg_escape_string($fN);
 		$l_name = pg_escape_string($lN);
-		if ($pwd_register == $pwd_check && isset($un) && isset($fN) && isset($lN) && isset($uImg)) {
+		if ($pwd_register == $pwd_check && isset($un) && isset($fN) && isset($lN)) {
 			$identification = '';
 			for ($i = 0; $i<7; $i++) 
 			{
 			    $identification .= mt_rand(0,9);
 			}
-			$query_register = "INSERT INTO userProfile (userID, username, password, firstN, lastN, proPicURL) VALUES ('" . $un_register . $identification . "', '" . $un_register . "', '" . $pwd_register . "', '" . $f_name . "', '" . $l_name . "', '" . $pro_pic . "')";
+			$query_register = "INSERT INTO userProfile (userID, username, password, firstN, lastN, proPicURL) VALUES ('" . $un_register . $identification . "', '" . $un_register . "', '" . $pwd_register . "', '" . $f_name . "', '" . $l_name . "', 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/608e1c34259817.56ca53c657e24.png')";
 			$db->exec($query_register);
 			setcookie("userId", $un_register . $identification);
 			header('Location: index.php');
