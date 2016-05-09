215ce3d4b902aea85e632e2235f3007bab9701f3
Tom Creagh <tomcreagh@outlook.com>
2016-05-10 11:02:03 +1200

    fixing various things

--- web/form.php
+++ web/form.php
@@ -110,7 +110,7 @@ if (isset($_POST['push'])){
         try {
             // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
             // $upload = $s3->upload($bucket, $nm, "L", 'public-read');
-            echo ($_FILES['userfile']['name'][$i]);
+            echo ($_FILES['userfile']['name'][0]);
         } catch(Exception $e) { 
             echo $e->getMessage();
             die();
