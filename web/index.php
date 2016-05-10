3c80b34a6ad9f5f193b2ebc5ecdb331e345b6c6f
Tom Creagh <tomcreagh@outlook.com>
2016-05-10 14:30:15 +1200

    fixing various things

--- web/index.php
+++ web/index.php
@@ -328,7 +328,7 @@ Post a hack
         <input name="hackTags[]" type='text'>
 
         <label>Title image:</label>
-        <input name="userfile[]" type="file" multiple>
+        <input name="userfile[]" type="file">
         
         <div class="steps">     
         </div>
