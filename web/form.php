9f7a18e33a24fb2d2ee911a8dde6cb4e4d4d2e7c
Tom Creagh <tomcreagh@outlook.com>
2016-05-10 14:40:06 +1200

    fixing various things

--- web/form.php
+++ web/form.php
@@ -41,7 +41,12 @@ if (isset($_POST['push'])){
             die();
         } else {
             try {
-                $upload = $s3->upload($bucket, $_FILES['userfile']['name'][$i], $_FILES['userfile']['tmp_name'][$i], 'public-read');
+                $upload = $s3->upload($bucket, $_FILES['userfile']['name'][$i], fopen($_FILES['userfile']['tmp_name'][$i], 'rb'), 'public-read');
+                // getCommand('PutObject', array(
+                //     'Bucket' => $bucket,
+                //     'Key'    => $_FILES['userfile']['name'][$i],
+                //     'Body'   => fopen('/tmp/photo02.jpg', 'r'),
+                //     ));
             } catch(Exception $e) { 
                 echo $e->getMessage();
                 die();
