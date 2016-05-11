09759ffe451e9d3fb55838701dc8bae9ed87c300
Tom Creagh <tomcreagh@outlook.com>
2016-05-11 16:04:44 +1200

    fixing various things

--- web/thesis.js
+++ web/thesis.js
@@ -77,7 +77,7 @@ $(window).load(function(){
 			$("body").removeClass( "offset4" );
 			$(".newHackFrame").removeClass( "offset7" );
 			$("body").scrollTop($("#" + hackID).offset().top - parseInt($(".navbutton").css("height")));
-			console.log($("#" + hackID + " .close").css("right", "0"););
+			console.log($("#" + hackID + " .close").css("right", "0"));
 		}
 		ihg(hackI);
 		// ajaxurl = 'index.php';
