11ec7ee0cbcf85407330b912afb5232fcbee9ef9
Tom Creagh <tomcreagh@outlook.com>
2016-05-17 09:33:24 +1200

    fixing various things

--- web/form.php
+++ web/form.php
@@ -4,33 +4,36 @@ ini_set('display_errors', 'On');
 require_once "dbconn.php";
 
 if (isset($_POST['push'])){
-    // $check = getimagesize($_FILES["userfile"]["tmp_name"]);
-    // $target_file = basename($_FILES["userfile"]["name"]);
-    $uploadOk = 1;
-    // $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-    // if($check !== false) {
-    //     echo "File is an image - " . $check["mime"] . ".";
-    //     $uploadOk = 1;
-    // } else {
-    //     echo "File is not an image.";
-    //     $uploadOk = 0;
-    // }
 
-    // Check file size
-    // if ($target_file > 5000000000) {
-    //     echo "Sorry, your file is too large.";
-    //     $uploadOk = 0;
-    // }
-    // Allow certain file formats
-    // if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    // && $imageFileType != "gif" ) {
-    //     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    //     $uploadOk = 0;
-    // }
     // foreach ($_FILES['userfile'] as $k => $v) {
 
     for ($i = 0; $i < count($_FILES['userfile']['name']); $i++) {
 
+    $check = getimagesize($_FILES["userfile"]["tmp_name"]);
+    $target_file = basename($_FILES["userfile"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+
+    //Check file size
+    if ($target_file > 5000000000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    
+    //Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
         if ($uploadOk == 0) {
             echo "Sorry, your file was not uploaded.";
             die();
