3aa670d9e2127b7384219de02f8ba56d921bb0b6
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-07-01 02:43:22 +1200

    bolstering

--- web/index.php
+++ web/index.php
@@ -377,7 +377,7 @@ Navigation Bar
          <div class="followedHacks">
         <?php
             for ($i = 0; $i < count($hacksGeneral['k']); $i++) {
-                if ($userFollowing['k'][$i]['userid'] == $_COOKIE["userId"]) {
+                if ($userFollowing['k'][$i]['following'] == $hacksGeneral['k'][$i]['hackid']) {
                     echo "<div class='hackSelectionFrameFollowedHacks' id='".$hacksGeneral['k'][$i]['hackid']."FollowedHacks'>";
                     echo "<img class='hackHeroImage' src='".$hacksGeneral['k'][$i]['heroimageurl']."'>";
                     echo '<div class="infoWrapperFollowedHacks"></div>';
