d3423f4da61a1a1bcd013e3ea2e2de819aeeab86
Tom Creagh <tomcreagh@outlook.com>
2016-07-04 11:32:22 +1200

    fixing various things

--- web/index.php
+++ web/index.php
@@ -327,7 +327,7 @@ Navigation Bar
             <?php
             for ($i = 0; $i < count($hacksGeneral['k']); $i++) {
                 if ($hacksGeneral['k'][$i]['userid'] == $_COOKIE["userId"]) {
-                    echo "<div class='hackSelectionFrameYourHacks' id='".$hacksGeneral['k'][$i]['hackid']."YourHacks'>";
+                    echo "<div class='hackSelectionFrameYourHacks' id='".$hacksGeneral['k'][$i]['hackid']."'>";
                     echo "<img class='hackHeroImageYourHacks' src='".$hacksGeneral['k'][$i]['heroimageurl']."'>";
                     echo '<div class="infoWrapperYourHacks"></div>';
                     echo '<h1 class="hackTitle">'.$hacksGeneral['k'][$i]['title'].'</h1>';
