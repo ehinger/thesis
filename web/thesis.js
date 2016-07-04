991887b8ab71c360dca8d3be00502d50e833961a
Tom Creagh <tomcreagh@outlook.com>
2016-07-04 12:50:25 +1200

    fixing various things

--- web/thesis.js
+++ web/thesis.js
@@ -196,9 +196,9 @@ $(window).load(function(){
 			$("#" + hackID + " .closeYourHacks").css("right", "0");
 			// $(".yourHacksMade").css("top", "10%");
 			$(".yourHacksMade").css("overflow", "hidden");
-			console.log($("#" + hackID ).position().top/10);
+			console.log($("#" + hackID ).position().top);
 			console.log("#" + hackID );
-			$(".yourHacksMade").scrollTop($("#" + hackID ).position().top/10);
+			$(".yourHacksMade").scrollTop($("#" + hackID ).position().top);
 			Cookies.set('followId', hackID);
 
 		}
