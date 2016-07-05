4765e4df478db6da3649a84b92ee8d40af6ba300
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-07-06 01:31:44 +1200

    bolstering

--- web/index.php
+++ web/index.php
@@ -575,7 +575,7 @@ for ($i = 0; $i < count($hacksGeneral['k']); $i++) {
         }
     }
     echo "<form class='follow' action='form.php' method='post'>";       
-    echo "<input class='follow' type='submit' value='Submit' name='follow'>";       
+    echo "<input type='submit' value='Submit' name='follow'>";       
     echo "</form>";   
     echo '</div>';
     echo "</div>";
