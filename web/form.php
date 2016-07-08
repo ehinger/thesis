2f16562d5c225b907b0d7b69dd15150884f99c19
Tom Creagh <tomcreagh@Toms-iMac.local>
2016-07-09 01:39:06 +1200

    bolstering

--- web/form.php
+++ web/form.php
@@ -67,41 +67,46 @@ if (isset($_POST['push'])){
 
     for ($i = 0; $i < count($_FILES['userfile']['name']); $i++) {
 
-    // $check = getimagesize($_FILES["userfile"]["tmp_name"][$i]);
-    // $target_file = basename($_FILES["userfile"]["name"][$i]);
+    $check = getimagesize($_FILES["userfile"]["tmp_name"][$i]);
+    $target_file = basename($_FILES["userfile"]["name"][$i]);
     $uploadOk = 1;
-    // $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     
-    // if($check !== false) {
-    //     echo "File is an image - " . $check["mime"] . ".";
-    //     $uploadOk = 1;
-    // } else {
-    //     echo "File is not an image.";
-    //     $uploadOk = 0;
-    // }
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
 
-    //Check file size
+    // Check file size
     // if ($target_file > 5000000000) {
     //     echo "Sorry, your file is too large.";
     //     $uploadOk = 0;
     // }
-            if ($uploadOk == 0) {
+    
+    //Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "JPG" && $imageFileType != "png" && $imageFileType != "PNG" && $imageFileType != "jpeg" && $imageFileType != "JPEG" && $imageFileType != "gif" && $imageFileType != "GIF" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+        if ($uploadOk == 0) {
             echo "Sorry, your file was not uploaded.";
             die();
         } else {
-            try {
-                $upload = $s3->upload($bucket, $_FILES['userfile']['name'][$i], fopen($_FILES['userfile']['tmp_name'][$i], 'rb'), 'public-read');
-            } catch(Exception $e) { 
-                echo $e->getMessage();
-                die();
-            } 
+                if (move_uploaded_file($_FILES["userfile"]["tmp_name"], $target_file)) {
+                    echo "The file ". basename( $_FILES["userfile"]["name"]). " has been uploaded.";
+                } else {
+                    echo "Sorry, there was an error uploading your file.";
+                }
+            // try {
+            //     $upload = $s3->upload($bucket, $_FILES['userfile']['name'][$i], fopen($_FILES['userfile']['tmp_name'][$i], 'rb'), 'public-read');
+            // } catch(Exception $e) { 
+            //     echo $e->getMessage();
+            //     die();
+            // } 
         }
-    //Allow certain file formats
-    // if($imageFileType != "jpg" && $imageFileType != "JPG" && $imageFileType != "png" && $imageFileType != "PNG" && $imageFileType != "jpeg" && $imageFileType != "JPEG" && $imageFileType != "gif" && $imageFileType != "GIF" ) {
-    //     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    //     $uploadOk = 0;
-    // }
-
     }
 
     try {
